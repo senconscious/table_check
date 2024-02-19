35d6de06ff54aab3e8f0c957d179a82c8bcc59ea
Artem Baikov <artem.baikov.darkmoon@gmail.com>
2024-02-19 13:50:20 +0300

    add filters by date and table id for listing reservations

--- lib/table_check/reservations.ex
+++ lib/table_check/reservations.ex
@@ -10,4 +10,6 @@ defmodule TableCheck.Reservations do
   defdelegate create_reservations(attrs), to: CreateReservationCommand, as: :execute
 
   defdelegate list_reservations, to: ReservationQuery, as: :list_all
+
+  defdelegate list_reservations(filters), to: ReservationQuery, as: :list_all
 end
--- lib/table_check/reservations/queries/reservation_query.ex
+++ lib/table_check/reservations/queries/reservation_query.ex
@@ -3,15 +3,45 @@ defmodule TableCheck.Reservations.ReservationQuery do
   Provides listing reservations
   """
 
+  import Ecto.Query
+
   alias TableCheck.Reservations.ReservationSchema
 
   alias TableCheck.Repo
 
+  @type filters :: %{
+          optional(:table_id) => integer(),
+          optional(:date) => Date.t()
+        }
+
   @doc """
-  List all reservations in system
+  List reservations in system.
+
+  ## Filters
+
+  - table_id - identifier of table for which list reservations. If not provided than lists
+        all reservations in system
+  - date - date when reservation started
   """
-  @spec list_all() :: [ReservationSchema.t()]
-  def list_all do
-    Repo.all(ReservationSchema)
+  @spec list_all(filters()) :: [ReservationSchema.t()]
+  def list_all(filters \\ %{})
+
+  def list_all(filters) do
+    ReservationSchema
+    |> by_table(filters)
+    |> by_date(filters)
+    |> Repo.all()
   end
+
+  defp by_table(query, %{table_id: table_id}) do
+    where(query, table_id: ^table_id)
+  end
+
+  defp by_table(query, _), do: query
+
+  defp by_date(query, %{date: date}) do
+    where(query, [reservation], fragment("?::date", reservation.start_at) == ^date)
+  end
+
+  defp by_date(query, _), do: query
 end
