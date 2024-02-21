0403dd936c634e6a14beef52435e1d6dc14fd6b9
Artem Baikov <artem.baikov.darkmoon@gmail.com>
2024-02-21 12:34:17 +0300

    move heavy patternmatching to separate functions in table query test

--- test/table_check/tables/queries/table_query_test.exs
+++ test/table_check/tables/queries/table_query_test.exs
@@ -13,9 +13,7 @@ defmodule TableCheck.Tables.TableQueryTest do
   end
 
   test "list_reserved_table_ids/1 returns appropriate table ids" do
-    %{guests: [guest], tables: [table]} =
-      restaurant =
-      insert!(:restaurant_with_tables_and_guests)
+    restaurant = insert!(:restaurant_with_tables_and_guests)
 
     assert [] ==
              TableQuery.list_reserved_table_ids(%{
@@ -24,6 +22,8 @@ defmodule TableCheck.Tables.TableQueryTest do
                restaurant_id: restaurant.id
              })
 
+    {guest, table} = pick_table_for_reservation(restaurant)
+
     insert!(:reservation,
       guest_id: guest.id,
       table_id: table.id,
@@ -31,21 +31,8 @@ defmodule TableCheck.Tables.TableQueryTest do
       end_at: today_at!(~T[22:00:00])
     )
 
-    # test overlapping
-    for {min_datetime, max_datetime} <- [
-          {
-            today_at!(~T[17:00:00]),
-            today_at!(~T[18:00:00])
-          },
-          {
-            today_at!(~T[22:00:00]),
-            today_at!(~T[23:00:00])
-          },
-          {
-            today_at!(~T[18:01:00]),
-            today_at!(~T[21:59:00])
-          }
-        ] do
+    # Test overlapping.
+    for {min_datetime, max_datetime} <- overlapping_datetimes() do
       assert [reserved_table_id] =
                TableQuery.list_reserved_table_ids(%{
                  min_datetime: min_datetime,
@@ -56,7 +43,7 @@ defmodule TableCheck.Tables.TableQueryTest do
       assert reserved_table_id == table.id
     end
 
-    # test not overlaping
+    # Test not overlaping.
 
     assert [] =
              TableQuery.list_reserved_table_ids(%{
@@ -64,6 +51,27 @@ defmodule TableCheck.Tables.TableQueryTest do
                max_datetime: today_at!(~T[17:59:59]),
                restaurant_id: restaurant.id
              })
+  end
+
+  defp pick_table_for_reservation(%{guests: [guest], tables: [table]}) do
+    {guest, table}
+  end
+
+  defp overlapping_datetimes do
+    [
+      {
+        today_at!(~T[17:00:00]),
+        today_at!(~T[18:00:00])
+      },
+      {
+        today_at!(~T[22:00:00]),
+        today_at!(~T[23:00:00])
+      },
+      {
+        today_at!(~T[18:01:00]),
+        today_at!(~T[21:59:00])
+      }
+    ]
   end
 
   defp today_at!(time) do
