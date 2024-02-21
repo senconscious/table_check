3bf6a347eccb72b3ee21f10d6478d4b5bc947f88
Artem Baikov <artem.baikov.darkmoon@gmail.com>
2024-02-21 12:24:48 +0300

    humanize test steps in listing available tables test

--- test/table_check/tables_test.exs
+++ test/table_check/tables_test.exs
@@ -3,26 +3,36 @@ defmodule TableCheck.TablesTest do
 
   alias TableCheck.Tables
 
-  test "list_available_tables/1 return appropriate tables" do
-    %{guests: [guest], tables: [available_table, reserved_table]} =
-      restaurant =
-      insert!(:restaurant_with_tables_and_guests, tables: [build(:table), build(:table)])
-
-    insert!(:reservation,
-      guest_id: guest.id,
-      table_id: reserved_table.id,
-      start_at: today_at!(~T[18:00:00]),
-      end_at: today_at!(~T[22:00:00])
-    )
+  test "list_available_tables/1" do
+    restaurant = create_restaurant_with_tables_and_guests()
+    {guest, table_to_reserve, available_table} = pick_table_for_reservation(restaurant)
+    reserve_table(guest, table_to_reserve, today_at!(~T[18:00:00]), today_at!(~T[22:00:00]))
 
     assert [actual_available_table] =
              Tables.list_available_tables(%{
-               min_datetime: today_at!(~T[18:00:00]),
-               max_datetime: today_at!(~T[22:00:00]),
+               min_datetime: today_at!(~T[17:00:00]),
+               max_datetime: today_at!(~T[21:00:00]),
                restaurant_id: restaurant.id
              })
 
     assert actual_available_table.id == available_table.id
+  end
+
+  defp create_restaurant_with_tables_and_guests do
+    insert!(:restaurant_with_tables_and_guests, tables: [build(:table), build(:table)])
+  end
+
+  defp pick_table_for_reservation(%{guests: [guest], tables: [table_to_reserve, available_table]}) do
+    {guest, table_to_reserve, available_table}
+  end
+
+  defp reserve_table(guest, table, start_at, end_at) do
+    insert!(:reservation,
+      guest_id: guest.id,
+      table_id: table.id,
+      start_at: start_at,
+      end_at: end_at
+    )
   end
 
   defp today_at!(time) do
